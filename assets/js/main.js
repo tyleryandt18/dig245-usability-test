cd25da4332aa783a813e451bcb8218a0066e334d
tyleryandt18 <tyyandt@davidson.edu>
2023-10-17 09:30:41 -0400

    Final Design

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,45 @@
-
 /* javascript */
+
+let myForm = document.querySelector("#formSUS");
+console.log(myForm);
+
+myForm.addEventListener("submit", (event) => {
+     // Prevent the default form submission behavior that scrolls to the top
+     event.preventDefault();
+
+    // declare variables for all the questions
+    let q1 = this.q1.value;
+    let q2 = this.q2.value;
+    let q3 = this.q3.value;
+    let q4 = this.q4.value;
+    let q5 = this.q5.value;
+    let q6 = this.q6.value;
+    let q7 = this.q7.value;
+    let q8 = this.q8.value;
+    let q9 = this.q9.value;
+    let q10 = this.q10.value;
+    
+    // test that the values are being read in properly
+    console.log(q1);
+    console.log(this.q1.value);
+
+    // from odd numbered questions, subtract 1
+    q1 -= 1;
+    q3 -= 1;
+    q5 -= 1;
+    q7 -= 1;
+    q9 -= 1;
+
+    // from even numbered questions, subtract the value from 5
+    q2 = 5 - q2;
+    q4 = 5 - q4;
+    q6 = 5 - q6;
+    q8 = 5 - q8;
+    q10 = 5 - q10;
+
+    // calculate the score, and store the variable
+    let answer = (q1 + q2 + q3 + q4 + q5 + q6 + q7 + q8 + q9 + q10) * 2.5;
+    this.answer.value = answer;
+
+    console.log(answer);
+});
